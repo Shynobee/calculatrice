0eacbda2cdc1f5a0a7259bb72dfb48d6b0ab05c7
Shynobee <w.khouaji@gmail.com>
2017-02-28 12:46:52 +0100

    fonction calcul simple

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ var calculatrice = function calculatrice() {
     "use strict";
     console.log("ready !!!");
     
-    var btn, saisie;
+    var btn, saisie2, saisie, add, sub, mult, div;
     
     btn = document.getElementById("btn");
     saisie = document.getElementById("ma_saisie");
+    saisie2 = document.getElementById("ma_saisie2")
     
     log(btn);
     log(saisie);
+    log(saisie2);
     
    // btn.onclick = function click() {
      //   log("click 1 !!!");
@@ -23,9 +25,15 @@ var calculatrice = function calculatrice() {
     function gererClicks() {
         log("click 2 !!!");
         log(saisie.value);
+        var res = add(saisie);
+        log(res)
     }
     
-    btn.onclick = gererClicks;
+    add = function add(a, b){
+        return a + b;
+    };
+    
+  //  btn.onclick = gererClicks;
     
     //btn.addEventListener("click", gererClick)   
 };
