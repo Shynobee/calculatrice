2541b9a3a012fa3e969163c3637efef7b6bd2fc5
Shynobee <w.khouaji@gmail.com>
2017-02-28 12:35:30 +0100

    ajout de la fonction calculette de base

---
+++ app.js
@@ -0,0 +1,31 @@
+var log = function log(data) {
+    "use strict"
+    window.console.log(data);
+}
+
+
+var calculatrice = function calculatrice() {
+    "use strict";
+    console.log("ready !!!");
+    
+    var btn, saisie;
+    
+    btn = document.getElementById("btn");
+    saisie = document.getElementById("ma_saisie");
+    
+    log(btn);
+    log(saisie);
+    
+   // btn.onclick = function click() {
+     //   log("click 1 !!!");
+    //};
+    
+    function gererClicks() {
+        log("click 2 !!!");
+        log(saisie.value);
+    }
+    
+    btn.onclick = gererClicks;
+    
+    //btn.addEventListener("click", gererClick)   
+};
